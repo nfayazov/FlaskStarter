c354e24ca524656f49c2cd471b3a68dee5f6fb80
Nadir Fayazov <nfayazov@gmail.com>
2018-01-04 23:56:04 -0800

    Added dependencies

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+#might as well right this in Node
 
-@app.route('/')
+from flask import Flask, render_template
+app = Flask(__name__, static_folder="static/dist", template_folder="static")
+
+@app.route("/"):
+   return render_template("index.html")
+
+@app.route('/hello')
 def hello_world():
    return('Hello, World!')
+
+if __name__=="__main__":
+   app.run()
